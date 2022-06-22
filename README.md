0705212c1f56a83e5a5c0ab58c214e2fff87ce07
ian mcgaunn <ian@mcgaunn.com>
2022-06-22 11:05:12 -0400

    start adding some basic examples

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dtcwithcpp
 exploring modern c++
+
+## index of experiments
+
+1. hello world - exp/hello/main.cpp
+2. structs - exp/structs/main.cpp
+3. classes - exp/classes/main.cpp
+4. prime_sieve - exp/prime_sieve/sieve.cpp
---
+++ exp/classes/simple.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+class Shape {
+public:
+	// this is a 'pure virtual' function.
+	virtual int Shape::GetArea() = 0;
+};
+
+class Rectangle {
+public:
+	Rectangle::Rectangle(int l, int w) {
+		this->length = l;
+		this->width = w;
+	}
+	int Rectangle::GetArea() {
+		return this->length * this->width;
+	}
+private:
+	int length = 0;
+	int width = 0;
+};
+
+class Square : public Rectangle {
+public:
+	Square::Square(int w) : Rectangle(w, w) {}
+};
+
+int main(int argc, char* argv[]) {
+	// create a 'Rectangle' object and a 'Square' object
+	// these are stack allocated in this case (no 'new')
+	Rectangle myRect = Rectangle(4, 9);
+	Square mySquare = Square(4);
+	
+	std::cout << "wow the area of the rect is: " << myRect.GetArea() << std::endl;
+	std::cout << "wow the area of the square is: " << mySquare.GetArea() << std::endl;
+
+	return 0;
+}
